269246c6cfe4e6391ca66bab0a3af44f5c9c6c1d
kimmustard <kimustard44@gmail.com>
2023-10-17 21:49:11 +0900

    설정완료

--- springProject/src/main/java/com/myweb/www/config/ServletConfiguration.java
+++ springProject/src/main/java/com/myweb/www/config/ServletConfiguration.java
@@ -1,5 +1,34 @@
 package com.myweb.www.config;
 
-public class ServletConfiguration {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.myweb.www.controller","com.myweb.www.handler"})
+public class ServletConfiguration implements WebMvcConfigurer{
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		//나중에 파일업로드 경로 추가
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver viewResolver =
+				new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		registry.viewResolver(viewResolver);
+	}
+	
+	//bean으로 multipartResolver를 설정하면 된다.
+
+	
 }
